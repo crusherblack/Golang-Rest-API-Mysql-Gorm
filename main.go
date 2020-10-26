1f7ff7ba94423e9d0781bccfe0df6a44743fad41
fadhil <fadhildarma13@gmail.com>
2020-10-26 23:55:53 +0700

    fix crud gorms

--- main.go
+++ main.go
@@ -22,20 +22,73 @@ type Booking struct {
 	Members int    `json:"members"`
 }
 
+// Result is an array of product
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to HomePage!")
 	fmt.Println("Endpoint Hit: HomePage")
 }
 
+func returnAllBookings(w http.ResponseWriter, r *http.Request) {
+	bookings := []Booking{}
+	db.Find(&bookings)
+
+	fmt.Println("Endpoint Hit: returnAllBookings")
+
+	res := Result{Code: 200, Data: bookings, Message: "Success get bookings"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func returnSingleBooking(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	var booking Booking
+	db.First(&booking, key)
+
+	fmt.Println("Endpoint Hit: Booking No:", key)
+
+	res := Result{Code: 200, Data: booking, Message: "Success get booking"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
 func createNewBooking(w http.ResponseWriter, r *http.Request) {
-	// get the body of our POST request
-	// return the string response containing the request body
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var booking Booking
 	json.Unmarshal(reqBody, &booking)
 	db.Create(&booking)
 	fmt.Println("Endpoint Hit: Creating New Booking")
-	json.NewEncoder(w).Encode(booking)
+
+	res := Result{Code: 200, Data: booking, Message: "Success create booking"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
 
 func updateBooking(w http.ResponseWriter, r *http.Request) {
@@ -51,24 +104,17 @@ func updateBooking(w http.ResponseWriter, r *http.Request) {
 	var booking Booking
 	db.Model(&booking).Where("id = ?", id).Updates(Booking{Id: id, User: newUser, Members: newMember})
 	fmt.Println("Endpoint Hit: Updating Booking")
-	json.NewEncoder(w).Encode(booking)
-}
 
-func returnAllBookings(w http.ResponseWriter, r *http.Request) {
-	bookings := []Booking{}
-	db.Find(&bookings)
-	fmt.Println("Endpoint Hit: returnAllBookings")
-	json.NewEncoder(w).Encode(bookings)
-}
+	res := Result{Code: 200, Data: booking, Message: "Success create booking"}
+	result, err := json.Marshal(res)
 
-func returnSingleBooking(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-	var booking Booking
-	db.First(&booking, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
-	fmt.Println("Endpoint Hit: Booking No:", key)
-	json.NewEncoder(w).Encode(booking)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
 
 func deleteBooking(w http.ResponseWriter, r *http.Request) {
@@ -78,7 +124,17 @@ func deleteBooking(w http.ResponseWriter, r *http.Request) {
 	db.Where("id = ?", key).Delete(&booking)
 
 	fmt.Println("Endpoint Hit: Delete Booking No:", key)
-	json.NewEncoder(w).Encode(booking)
+
+	res := Result{Code: 200, Data: booking, Message: "Success delete booking"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
 
 func handleRequests() {
@@ -86,12 +142,31 @@ func handleRequests() {
 	log.Println("Quit the server with CONTROL-C.")
 	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		res := Result{Code: 404, Message: "Method not found"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
+	myRouter.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		res := Result{Code: 403, Message: "Method not allowed"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/new-booking", createNewBooking).Methods("POST")
-	myRouter.HandleFunc("/all-bookings", returnAllBookings)
-	myRouter.HandleFunc("/booking/{id}", returnSingleBooking)
-	myRouter.HandleFunc("/update-booking/{id}", updateBooking).Methods("PUT")
-	myRouter.HandleFunc("/delete-booking/{id}", deleteBooking).Methods("DELETE")
+	myRouter.HandleFunc("/bookings", returnAllBookings).Methods("GET")
+	myRouter.HandleFunc("/booking/{id}", returnSingleBooking).Methods("GET")
+	myRouter.HandleFunc("/booking", createNewBooking).Methods("POST")
+	myRouter.HandleFunc("/booking/{id}", updateBooking).Methods("PUT")
+	myRouter.HandleFunc("/booking/{id}", deleteBooking).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
